49bb38affcab7448cc8f70f4c583fef71b9f860b
jgoedmakers <89616051+jgoedmakers@users.noreply.github.com>
2022-03-28 00:02:44 +0000

    Add files via upload

--- Key Generation Part 1.cpp
+++ Key Generation Part 1.cpp
@@ -10,7 +10,9 @@
 #include <cmath>
 #include <random>
 
+#include "crypto++/cryptlib.h"
 #include "crypto++/shake.h"
+#include "crypto++/filters.h"
 using CryptoPP::SHAKE128;
 
 
@@ -39,13 +41,21 @@ int main(int argc, char** argv) {
     //Step 2 - A = gen(seed_A) ∈ R_q^l×l    Generates a pseudorandom matrix using seed A and SHAKE-128.
 
     //Using SHAKE128 from crypto++, this only generates a SHAKE128 hash, and not a psuedorandom matrix.
+    std::string digest;
 
     SHAKE128 hash;
     //something wrong with this line below:
-    //hash.Update((const byte*)seed_A.data(), seed_A.size());
+    hash.Update((const CryptoPP::byte*)seed_A.data(), seed_A.size());
+    digest.resize(hash.DigestSize());
+    hash.Final((CryptoPP::byte*)&digest[0]);
+
+    CryptoPP::StringSource(digest, true);   //double check this part for proper arguments (encoder is currently default value)
 
     //using the shake128 string, a matrix sized lxl is populated and set to variable A
-    
+
+    //the matrix is 2 dimensional, so nested for loops are used
+    //for i in range l
+        //for j in range l
 
     //Step 3 - r = U({0,1}^256)     Generates a 256 byte key consisting of 1s and 0s (Uniform distribution).
     //The process is the same as step 1.
@@ -59,15 +69,26 @@ int main(int argc, char** argv) {
         }
     }
 
+    
     //test seed generation
     cout << "[";
     for(int i = 0; i < r.size(); i++) {
         cout << r[i] << ", ";
     }
     cout << "]" << endl;
+    
 
     //test shake128
-    
+    std::cout << "Digest: ";
+    for (unsigned char c : digest){
+            printf("%x",c);        
+            }
+    std::cout << std::endl;
+
 
     return 0;
 }
+
+
+//g++ -I/usr/local/include myprog1.cpp -o myprog1.out -L/usr/local/lib -lcryptopp
+//./myprog1.out 
