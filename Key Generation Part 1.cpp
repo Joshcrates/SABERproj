baa2e61221c827dfcca8197b7912b7d22c4db85e
jgoedmakers <89616051+jgoedmakers@users.noreply.github.com>
2022-04-25 20:37:09 -0400

    Add files via upload

--- Key Generation Part 1.cpp
+++ Key Generation Part 1.cpp
@@ -1,5 +1,11 @@
 //Key Generation Part 1 - James Goedmakers
 
+/**
+This is part 1 of the SABER Key Generation module, 
+covering lines 1-3 of the module algorithm steps
+specified in the SABER spec document.
+**/
+
 #include <cstdlib>
 #include <iostream>
 #include <string>
@@ -14,6 +20,9 @@
 #include "crypto++/cryptlib.h"
 #include "crypto++/shake.h"
 #include "crypto++/filters.h"
+#include "crypto++/hex.h"
+#include "crypto++/files.h"
+
 using CryptoPP::SHAKE128;
 
 const int l = 2;
@@ -23,14 +32,15 @@ const int eq = 12;  //check later if 13 or 12
 using namespace std;
 
 int main(int argc, char** argv) {
+    using namespace CryptoPP;
 
+    //******************************STEP 1******************************
     //Step 1 - seed_A ← U({0,1}^256)
     //Generates a 256 byte seed consisting of 1s and 0s (Uniform).
-
     //setup to generate uniform distribution of 1s and 0s
     random_device rd;  //Will be used to obtain a seed for the random number engine
     mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    uniform_int_distribution<> distrib(0, 1);
+    uniform_int_distribution<> distrib(0, 1); //uniform distribution as specified by the algorithm
 
     //generate seed_A
     vector<int> seed_A;
@@ -41,82 +51,56 @@ int main(int argc, char** argv) {
             seed_A.push_back(distrib(gen));
         }
     }
-    
+    //************************************************************
 
-
+    //******************************STEP 2******************************
     //Step 2 - A = gen(seed_A) ∈ R_q^l×l    
     //Generates a pseudorandom matrix using seed A and SHAKE-128.
-
-    //Using SHAKE128 from crypto++, this only generates a SHAKE128 hash, and not a psuedorandom matrix.
-    std::string digest;
-
-    SHAKE128 hash;
-    hash.Update((const CryptoPP::byte*)seed_A.data(), seed_A.size());
-    digest.resize(hash.DigestSize());
-    hash.Final((CryptoPP::byte*)&digest[0]);
-
-    CryptoPP::StringSource(digest, true);   //double check this part for proper arguments (encoder is currently default value)
-
     //using the shake128 string, a matrix sized lxl is populated and set to variable A:
-
     //Matrix generation:
     //Instantiate byte string object buf of length l^2 × n × ϵq/8
-    vector<char> buf(l*l*n*eq/8,'0');
-    
-    //generate seed_B
-    vector<int> seed_B;
-    //loop to generate 256 bytes
-    for(int i = 0; i < l*l*n*eq/8; i++) {
-        //loop to generate 8 bits for each byte
-        for(int j = 0; j < 8; j++) {
-            seed_B.push_back(distrib(gen));
-        }
-    }
+    //vector<char> buf(l*l*n*eq/8,'0');
+    std::string buf;
 
+    //Using SHAKE128 from crypto++, this only generates a SHAKE128 hash, and not a psuedorandom matrix.
+    SHAKE128 hash;
     //SHAKE-128(buf, l^2 × n × ϵq/8, seedAAA, SABER SEEDBYTES)
-    hash.Update((const CryptoPP::byte*)seed_B.data(), seed_B.size());
+    hash.Update((const byte*)seed_A.data(), seed_A.size());
     //buf.resize(hash.DigestSize());
+    buf.resize(32);
     hash.Final((CryptoPP::byte*)&buf[0]);
-
-    CryptoPP::StringSource(&buf[0], true);
-    string buf_as_a_string(buf.begin(),buf.end());
-    cout << "buf_as_a_string size:" << buf_as_a_string.size() << endl;
-    cout << "seed_B size:"<< seed_B.size() << endl;
+    //CryptoPP::StringSource(&buf[0], true);
+    //string buf_as_a_string(buf.begin(),buf.end());
+    //CryptoPP::HexEncoder encoder;
+    CryptoPP::HexEncoder encoder(new FileSink(std::cout));
+    //StringSource(buf, true, new Redirector(encoder));
 
     //Split buf into l^2 × n equal bit strings of bit length ϵq and obtain
     //(bufl2n−1 ‖ . . . ‖ buf0) = buf
-    vector<bitset<eq>> buf_splitted(l*l*n,'0');
+    //vector<bitset<eq>> buf_splitted(l*l*n,'0');
+    vector<bitset<eq>> buf_splitted;
     for(int i = 0; i < l*l*n; i++) {
-        buf_splitted[i] = bitset<eq>(buf_as_a_string.c_str()[i]);
+        buf_splitted.push_back(bitset<eq>(buf[i]));
     }
 
-    cout << buf_splitted[0] << endl;
-    cout << buf_splitted[l*l*n-1] << endl;  //check the bit/byte lengths, why are these all zeros?
-    //exit(0);
     //the matrix is 2 dimensional, so nested for loops are used
+    //matrix_A is filled with buf splits
     int matrix_A[l][l][n];
     int k = 0;
     for(int i_1 = 0; i_1 < l; i_1++) {
         for(int i_2 = 0; i_2 < l; i_2++) {
             for(int j = 0; j < n; j++) {
-                //printf("x");
-                //matrix_A[i_1][i_2][j] = 1;
                 matrix_A[i_1][i_2][j] = buf_splitted[k].to_ulong();
                 k++;
             }
         }
     }
+    //************************************************************
+    
 
-    for(int i_1 = 0; i_1 < l; i_1++) {
-        for(int i_2 = 0; i_2 < l; i_2++) {
-            cout << matrix_A[i_1][i_2] << " ";
-        } 
-        cout << endl;
-    }
-    //exit(0);
+    //******************************STEP 3******************************
     //Step 3 - r = U({0,1}^256)     
     //Generates a 256 byte key consisting of 1s and 0s (Uniform distribution).
-    
     //The process is the same as step 1.
     vector<int> r;
     //loop to generate 256 bytes
@@ -126,29 +110,49 @@ int main(int argc, char** argv) {
             r.push_back(distrib(gen));
         }
     }
+    //************************************************************
 
-    
-    //test seed generation
+    //******************************TESTS******************************
+    //test seed generation from line 1 or 3
+    cout << "Testing Seed Generation:" << endl;
     cout << "[";
-    for(int i = 0; i < r.size(); i++) {
-        cout << r[i] << ", ";
+    for(int i = 0; i < seed_A.size(); i++) {
+        cout << seed_A[i];
     }
-    cout << "]" << endl;
+    cout << "]" << endl << endl;
     
-
     //test shake128
-    std::cout << "Digest: ";
-    for (unsigned char c : digest){
-            printf("%x",c);        
-            }
-    std::cout << std::endl;
-
-    //test shake128
+    cout << "Testing SHAKE128 Output:" << endl;
     std::cout << "Buf: ";
     for (unsigned char c : buf){
             printf("%x",c);        
             }
     std::cout << std::endl;
+    cout << endl;
+
+    //test matrix generation from line 2 (per matrix element)
+    cout << "Testing Matrix A Generation:" << endl;
+    for(int i_1 = 0; i_1 < l; i_1++) {
+        for(int i_2 = 0; i_2 < l; i_2++) {
+            cout << matrix_A[i_1][i_2] << " ";
+        } 
+        cout << endl;
+    }
+    cout << endl;
+
+    /**
+    //test matrix generation from line 2 (per polynomial)
+    for(int i_1 = 0; i_1 < l; i_1++) {
+        for(int i_2 = 0; i_2 < l; i_2++) {
+            for(int j = 0; j < n; j++) {
+                cout << matrix_A[i_1][i_2][j] << " ";
+            }
+            cout << endl;
+        } 
+        cout << endl;
+    }
+    **/
+    //************************************************************
 
     return 0;
 }
